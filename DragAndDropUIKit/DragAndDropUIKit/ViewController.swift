ed190ce0ef7ac852f94174c01e4f1229e6711125
五十嵐伸雄 <gonnbe1106@gmail.com>
2024-02-18 13:16:18 +0900

    create project

---
+++ DragAndDropUIKit/DragAndDropUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragAndDropUIKit
+//
+//  Created by 五十嵐伸雄 on 2024/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
